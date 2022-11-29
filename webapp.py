8c1fe34c68f2cad8a1d36f6406f303ff6ab8a1a3
Alphire <alphire22@gmail.com>
2022-11-29 11:07:12 +0800

    latest version

--- webapp.py
+++ webapp.py
@@ -79,7 +79,7 @@ if selected == 'Home':
     dataset = st.container()
 
     with header:
-        st.title('alphire pogi hehe')
+        st.title('alphire pogi hehe latest version')
         st.header("this is a machine learning program for predicting the price of california houses")
 
     with dataset:
